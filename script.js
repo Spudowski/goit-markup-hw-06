4ef3b7208275a6b4ebf6f7957dcf0fbbe0b6cb44
Spudowski <adriandobrowolski12@gmail.com>
2024-03-19 19:52:23 +0100

    hw6

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuOpen = document.getElementById('open-menu');
+const menuClose = document.getElementById('close-menu');
+const sidebar = document.querySelector('.sidebar');
+
+menuOpen.addEventListener('click', () => sidebar.style.left = '0');
+menuClose.addEventListener('click', () => sidebar.style.left = '-120%')
