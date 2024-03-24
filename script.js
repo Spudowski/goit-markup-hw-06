b42d2a1bc7fa950d0a2d34f777ad3cd8c74ae881
Spudowski <adriandobrowolski12@gmail.com>
2024-03-24 12:28:39 +0100

    hw6

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ const menuClose = document.getElementById('close-menu');
 const sidebar = document.querySelector('.sidebar');
 
 menuOpen.addEventListener('click', () => sidebar.style.left = '0');
-menuClose.addEventListener('click', () => sidebar.style.left = '-120%')
+menuClose.addEventListener('click', () => sidebar.style.left = '-200%')
